c01752170ab415528cd53eaaa64a43b9549da778
ssohy <sohyeonj415@gmail.com>
2024-01-24 12:09:49 +0900

    feat: 벽 설정

--- The_Samurai_of_Honor/Assets/Scripts/Player.cs
+++ The_Samurai_of_Honor/Assets/Scripts/Player.cs
@@ -32,57 +32,7 @@ public class Player : MonoBehaviour
         moveV = new Vector3(h, 0, v).normalized;
 
         transform.position += moveV * moveSpeed * Time.deltaTime;
-        /*
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-        }*/
-
-
-        /*벡터의 정규화
-        float h, v;
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
-
-        Vector3 moveDir = Vector3.forward * v + Vector3.right * h;
-
-        Rigidbody rb = player.GetComponent<Rigidbody>();
-        rb.MovePosition(rb.position + moveDir.normalized * moveSpeed * Time.deltaTime);*/
-
-
-        /*if (Input.GetKey(KeyCode.A))
-        {
-            player.transform.Translate(new Vector3(-1 , 0, 0).normalized * Time.deltaTime * moveSpeed);
-            Debug.Log(new Vector3(-1, 0, 0).normalized * Time.deltaTime * moveSpeed);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            player.transform.Translate(new Vector3(1 , 0, 0).normalized * Time.deltaTime * moveSpeed);
-        }
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            player.transform.Translate(new Vector3(0, 0, 1 ).normalized * Time.deltaTime * moveSpeed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            player.transform.Translate(new Vector3(0, 0, -1 ).normalized * Time.deltaTime * moveSpeed);
-        }*/
-
-
+        
     }
 
     //#.공격
