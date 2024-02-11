18df4cb1e46b1a9e6ff287d2f2e4e0825e990c8b
ssohy <sohyeonj415@gmail.com>
2024-02-11 21:28:45 +0900

    fix: 싱글톤적용2

--- The_Samurai_of_Honor/Assets/Scripts/DataManager.cs
+++ The_Samurai_of_Honor/Assets/Scripts/DataManager.cs
@@ -6,24 +6,22 @@ public class DataManager : MonoBehaviour
 {
     private static DataManager instance;
 
-
     // 데이터 변수
     public int playerHp;
     public int enemyCnt;
     public int currentDay;
     public bool isDay;
+
     // 싱글톤 인스턴스 가져오기
     public static DataManager Instance
     {
         get
         {
-            // 인스턴스가 없는 경우 새로 생성
             if (instance == null)
             {
-                // 새 GameObject 생성
                 GameObject singletonObject = new GameObject("DataManager");
                 instance = singletonObject.AddComponent<DataManager>();
-                DontDestroyOnLoad(singletonObject); // 씬 전환 시 파괴되지 않도록 설정
+                DontDestroyOnLoad(singletonObject);
             }
             return instance;
         }
@@ -37,20 +35,18 @@ public class DataManager : MonoBehaviour
 
     private void Awake()
     {
-        // 싱글톤 패턴 구현
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject); // 씬 전환 시에도 유지되도록 설정
-            InitializeData(); // 데이터 초기화 함수 호출
+            DontDestroyOnLoad(gameObject);
+            InitializeData();
         }
         else
         {
-            Destroy(gameObject); // 이미 다른 DataManager 인스턴스가 있다면 파괴
+            Destroy(gameObject);
         }
     }
 
-    // 데이터 초기화
     private void InitializeData()
     {
         playerHp = 100;
